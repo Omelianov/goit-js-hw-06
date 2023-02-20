ed528f514951f47111f3c2a81c7f8d5dbb4338b7
Omelianov Nicolai <omelianov.n@gmail.com>
2023-02-21 01:56:10 +0200

    added task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
 
+const form = document.querySelector('.login-form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const elements = form.elements;
+    const emailValue = elements.email.value.trim();
+    const passwordValue = elements.password.value.trim();
+
+    if (!emailValue || !passwordValue) {
+        alert('All fields must be filled!');
+        return;
+    }
+
+    const data = {
+        emailValue,
+        passwordValue
+    };
+
+    console.log(data);
+    form.reset();
+});
+
+
+
+
+
+
+
