b7b51010d108b06d95d94f02adf7ac23656432d4
Omelianov Nicolai <omelianov.n@gmail.com>
2023-02-20 15:51:42 +0200

    added tasks 1 and 2

---
+++ src/js/task-01.js
@@ -0,0 +1,11 @@
+const categoriesList = document.querySelector('#categories');
+const items = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach(item => {
+    const categoryName = item.querySelector('h2');
+    const categoryElements = item.querySelectorAll('li');
+    console.log(`Category: ${categoryName.textContent}`);
+    console.log(`Elements: ${categoryElements.length}`);
+});
---
+++ src/js/task-02.js
@@ -0,0 +1,25 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+console.log(ingredients);
+
+
+
+const categoriesList = document.querySelector('#ingredients');
+const liArr = [];
+
+ingredients.forEach(ingredient => {
+  const createLi = document.createElement('li')
+  createLi.className = 'item'
+  createLi.textContent = ingredient;
+  liArr.push(createLi)
+
+})
+categoriesList.append(...liArr)
+
