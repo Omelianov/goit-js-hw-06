dc3df1b316f90add92bdda51fd7c8b32fc5ff327
Omelianov Nicolai <omelianov.n@gmail.com>
2023-02-21 00:57:18 +0200

    added task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
+
+const addClass = document.body.classList.add('main');
+const bgBodyClass = document.querySelector('.main')
+const SpanColor = document.querySelector('.color')
+const btnColor = document.querySelector('.change-color')
+
+
+btnColor.addEventListener('click', () => {
+  bgBodyClass.style.backgroundColor = `${getRandomHexColor()}`
+  SpanColor.textContent = getRandomHexColor()
+})
+
+
+
